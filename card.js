53dd310ce63f3e82e30acd851c240b8c70018d68
ashyLohia <ashish.kumar@qsstechnosoft.com>
2022-04-06 03:20:02 -0600

    payment gateway testing

---
+++ card.js
@@ -0,0 +1,11 @@
+// const appId = 'sandbox-sq0idb-GbaS8M85y9qpI36ATHtPWA';
+// const locationId = 'L877QJMHRDAG8';
+
+// const payments = Square.payments(appId, locationId);
+// const card = await payments.card();
+// await card.attach('#card');
+// const form = document.querySelector('#card-payment');
+// form.addEventListener('submit', async (event) => {
+//     event.preventDefault();
+//     const result = await card.tokenize(); // the card nonce
+// });
